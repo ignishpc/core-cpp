f40f4dbf32e299e2b95fcfb73347b26b819d69dd
César Pomar <cesarpomar18@gmail.com>
2020-02-12 14:04:38 +0100

    solve "double free or corruption" error

--- src/ignis/executor/core/modules/IModule.cpp
+++ src/ignis/executor/core/modules/IModule.cpp
@@ -22,20 +22,18 @@ std::shared_ptr<ignis::executor::core::selector::ITypeSelector> IModule::typeFro
     return executor_data->getType(type.getStandardName());
 }
 
-std::string BASIC_NAMED_TYPES[]{
-        RTTInfo::from<void>().getStandardName(),//I_VOID = 0x0,
-        RTTInfo::from<bool>().getStandardName(),//I_BOOL = 0x1,
-        RTTInfo::from<int8_t>().getStandardName(),//I_I08 = 0x2,
-        RTTInfo::from<int16_t>().getStandardName(),//I_I16 = 0x3,
-        RTTInfo::from<int32_t>().getStandardName(),//I_I32 = 0x4,
-        RTTInfo::from<int64_t>().getStandardName(),//I_I64 = 0x5,
-        RTTInfo::from<double>().getStandardName(),//I_DOUBLE = 0x6,
-        RTTInfo::from<std::string>().getStandardName(),//I_STRING = 0x7,
-};
-
-int N_NAMED_TYPES = 8;
-
 std::shared_ptr<ignis::executor::core::selector::ITypeSelector> IModule::typeFromHeader(const std::string &header) {
+    const int N_NAMED_TYPES = 8;
+    const static std::string BASIC_NAMED_TYPES[]{
+            RTTInfo::from<void>().getStandardName(),//I_VOID = 0x0,
+            RTTInfo::from<bool>().getStandardName(),//I_BOOL = 0x1,
+            RTTInfo::from<int8_t>().getStandardName(),//I_I08 = 0x2,
+            RTTInfo::from<int16_t>().getStandardName(),//I_I16 = 0x3,
+            RTTInfo::from<int32_t>().getStandardName(),//I_I32 = 0x4,
+            RTTInfo::from<int64_t>().getStandardName(),//I_I64 = 0x5,
+            RTTInfo::from<double>().getStandardName(),//I_DOUBLE = 0x6,
+            RTTInfo::from<std::string>().getStandardName(),//I_STRING = 0x7,
+    };
     IGNIS_LOG(info) << "Cheeking incoming partition type";
     std::string type_name = "";
     try {
