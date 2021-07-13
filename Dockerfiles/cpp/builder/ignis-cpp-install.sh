1782ae597385581ed96d68d8bc93ce09363bab52
César Pomar <cesarpomar18@gmail.com>
2021-07-13 04:05:26 +0200

    Dockerfiles for the new build system

---
+++ Dockerfiles/cpp/builder/ignis-cpp-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+apt update
+apt -y install g++
+rm -rf /var/lib/apt/lists/*
+
+cp -R ${IGNIS_HOME}/core/cpp/lib/* ${IGNIS_HOME}/lib
+rm -fR ${IGNIS_HOME}/core/cpp/lib
+rm -fR ${IGNIS_HOME}/core/cpp/include
+ldconfig
